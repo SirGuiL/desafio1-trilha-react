e8f2dde19f12246442cdb629a97c58e286bf3770
Guilherme Lima de Souza <guibiel-10@hotmail.com>
2022-06-16 23:28:48 -0300

    finish challenge

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,14 +16,32 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if (!newTaskTitle) return
+
+    const newTask = {
+      id: Math.floor(Math.random() * (9999 - 1) + 1),
+      title: newTaskTitle,
+      isComplete: false
+    }
+
+    setTasks(old => [...old, newTask]);
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const filteredTask = tasks.map(task => task.id === id ? {
+      ...task,
+      isComplete: !task.isComplete
+    } : task);
+
+    setTasks(filteredTask)
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    const newTasks = tasks.filter(taskid => taskid.id !== id)
+
+    setTasks(newTasks)
   }
 
   return (
